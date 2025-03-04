d98cadf58746a0a800f4ea8cdf0e8f72f198d923
agomezomil <alejo.gomez@sancorsalud.com.ar>
2025-03-04 07:34:59 -0300

    fix mensajes

--- routing/routes/simple_route.py
+++ routing/routes/simple_route.py
@@ -1,12 +1,62 @@
 # simple_route.py
-from fastapi import APIRouter
-todo_router = APIRouter()
+from fastapi import APIRouter, Path
+from model import Todo, TodoItem
 
+todo_router = APIRouter()
 todo_list = []
 @todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
-    return {"mensaje": "Todo agregado exitosamente"}
+    return {
+        "mensaje": "Todo agregado exitosamente."
+    }
+
 @todo_router.get("/todo")
-async def retrieve_todos() -> dict:
-    return {"todos": todo_list}
+async def retrieve_todo() -> dict:
+   return {
+     "todos": todo_list
+    }
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(title="El ID de Todo a buscar")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+           return {
+              "todo": todo
+            }
+    return {
+       "mensaje": "Todo con ID provisto no existe."
+    }
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(title="El ID de Todo para actualizar")) -> dict:
+    for todo in todo_list:
+       if todo.id == todo_id:
+          todo.item = todo_data.item
+          return {
+              "mensaje": "Todo fue actualizado exitosamente."
+              }
+    return {
+        "mensaje": "Todo con ID provisto no existe."
+    }
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                "mensaje": "Todo borrado exitosamente."
+            }
+    return {
+        "mensaje": "Todo con ID provisto no existe."
+    }
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
+    return {
+        "mensaje": "Todos borrados exitosamente."
+    }
